73be205c45caea48a4427ff4ab02abae072f4998
jack <jsupdkd352@gmail.com>
2023-06-22 16:07:12 -0400

    Add navbar

---
+++ components/Nav.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt={"Share Prompts Logo"}
+          width={32}
+          height={32}
+          className="object-contain"
+        />
+        <p className="logo_text">Share Prompts</p>
+      </Link>
+
+      {/* mobile navigation */}
+      <div className="sm:flex hidden gap-4">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button
+              type="button"
+              className="outline_btn"
+              onClick={() => signOut}
+            >
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt={"Profile"}
+                width={37}
+                height={37}
+                className="object-contain"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
